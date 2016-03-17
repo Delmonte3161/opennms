f0917061d468378467ae5f0e20dfab0b6d00f0c9
Deepak Patel <deepak.patel@cerner.com>
2016-03-17 16:21:46 +0530

    Updating getters & setters - JIRA # HZN-609

--- features/events/traps/src/main/java/org/opennms/netmgt/trapd/TrapdConfigBean.java
+++ features/events/traps/src/main/java/org/opennms/netmgt/trapd/TrapdConfigBean.java
@@ -48,56 +48,40 @@ public class TrapdConfigBean implements TrapdConfig{
 	private boolean m_newSuspectOnTrap;
 	private List<SnmpV3User> m_snmpV3Users;
 	
-	public String getM_snmpTrapAddress() {
-		return m_snmpTrapAddress;
+	public void setSnmpTrapAddress(String snmpTrapAddress) {
+		this.m_snmpTrapAddress = snmpTrapAddress;
 	}
-
-	public void setM_snmpTrapAddress(String m_snmpTrapAddress) {
-		this.m_snmpTrapAddress = m_snmpTrapAddress;
+	
+	public void setSnmpTrapPort(int snmpTrapPort) {
+		this.m_snmpTrapPort = snmpTrapPort;
 	}
-
-	public int getM_snmpTrapPort() {
-		return m_snmpTrapPort;
+	
+	public void setNewSuspectOnTrap(boolean newSuspectOnTrap) {
+		this.m_newSuspectOnTrap = newSuspectOnTrap;
 	}
-
-	public void setM_snmpTrapPort(int m_snmpTrapPort) {
-		this.m_snmpTrapPort = m_snmpTrapPort;
-	}
-
-	public boolean isM_newSuspectOnTrap() {
-		return m_newSuspectOnTrap;
-	}
-
-	public void setM_newSuspectOnTrap(boolean m_newSuspectOnTrap) {
-		this.m_newSuspectOnTrap = m_newSuspectOnTrap;
-	}
-
-	public List<SnmpV3User> getM_snmpV3Users() {
-		return m_snmpV3Users;
-	}
-
-	public void setM_snmpV3Users(List<SnmpV3User> m_snmpV3Users) {
-		this.m_snmpV3Users = m_snmpV3Users;
+	
+	public void setSnmpV3Users(List<SnmpV3User> snmpV3Users) {
+		this.m_snmpV3Users = snmpV3Users;
 	}
 
 	@Override
 	public String getSnmpTrapAddress() {
 		return m_snmpTrapAddress;
 	}
-
+	
 	@Override
 	public int getSnmpTrapPort() {
 		return m_snmpTrapPort;
 	}
-
+	
 	@Override
 	public boolean getNewSuspectOnTrap() {
 		return m_newSuspectOnTrap;
 	}
-
+	
 	@Override
 	public List<SnmpV3User> getSnmpV3Users() {
 		return m_snmpV3Users;
 	}
-
+	
 }
