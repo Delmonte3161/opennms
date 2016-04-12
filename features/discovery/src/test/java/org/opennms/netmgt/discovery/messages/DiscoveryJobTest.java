7931323fd04a5c5db5a8f021a4413a2fad0d9c9d
Seth Leger <seth@opennms.org>
2016-04-12 14:49:13 -0400

    Added more tests for DiscoveryJob.calculateTaskTimeout().

--- features/discovery/src/test/java/org/opennms/netmgt/discovery/messages/DiscoveryJobTest.java
+++ features/discovery/src/test/java/org/opennms/netmgt/discovery/messages/DiscoveryJobTest.java
@@ -29,7 +29,7 @@
 package org.opennms.netmgt.discovery.messages;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,11 +49,62 @@ public class DiscoveryJobTest {
         DiscoveryJob discoveryJob = new DiscoveryJob(m_ranges, "Bogus FS", "Bogus Location");
         // Each task is taking 750 ms so totalTaskTimeout = 750 ms * 3 (number of tasks) = 2250 ms
         assertEquals(Math.round(2250 * 1.5), discoveryJob.calculateTaskTimeout()); 
-        assertFalse(discoveryJob.calculateTaskTimeout() > Integer.MAX_VALUE);
     }
 
     @Test
-    public void testTimeoutLargerThanIntegerMaxValue() throws Exception {
-        // TODO: Write this test
+    public void testTimeoutIntegerMaxValueMinus1() throws Exception {
+        List<IPPollRange> m_ranges = new ArrayList<IPPollRange>();
+
+        // Minimum value that should be smaller than Integer.MAX_VALUE:
+        //
+        // (Integer.MAX_VALUE - 1) * timeout * retries * fudge_factor / fudge_factor
+        //       (2^31 - 1)        *    1    *    1    *     1.5      /     1.5
+        //  ((2^31 - 1) / 3 * 2)   *    1    *    1    *     1.5
+
+        // Start the range at 0.0.0.1 so that we don't count zero as a value
+        IPPollRange ipPollRange = new IPPollRange("0.0.0.1", "85.85.85.84", 1, 0);
+        m_ranges.add(ipPollRange);
+        DiscoveryJob discoveryJob = new DiscoveryJob(m_ranges, "Bogus FS", "Bogus Location");
+        assertEquals(Integer.MAX_VALUE - 1, discoveryJob.calculateTaskTimeout());
+    } 
+
+    @Test
+    public void testTimeoutIntegerMaxValue() throws Exception {
+        List<IPPollRange> m_ranges = new ArrayList<IPPollRange>();
+
+        // Start the range at 0.0.0.1 so that we don't count zero as a value
+        IPPollRange ipPollRange = new IPPollRange("0.0.0.1", "85.85.85.85", 1, 0);
+        m_ranges.add(ipPollRange);
+        DiscoveryJob discoveryJob = new DiscoveryJob(m_ranges, "Bogus FS", "Bogus Location");
+        assertEquals(Integer.MAX_VALUE, discoveryJob.calculateTaskTimeout());
+    } 
+
+    @Test
+    public void testTimeoutLargerThanIntegerMaxValue1() throws Exception {
+        List<IPPollRange> m_ranges = new ArrayList<IPPollRange>();
+        IPPollRange ipPollRange = new IPPollRange("0.0.0.0", "89.0.0.0", 1, 0);
+        m_ranges.add(ipPollRange);
+        DiscoveryJob discoveryJob = new DiscoveryJob(m_ranges, "Bogus FS", "Bogus Location");
+        assertEquals(Integer.MAX_VALUE, discoveryJob.calculateTaskTimeout());
+    } 
+
+    @Test
+    public void testTimeoutLessThanIntegerMaxValueWithDefaultTimeoutRetries() throws Exception {
+        List<IPPollRange> m_ranges = new ArrayList<IPPollRange>();
+        // TODO: Replace defaults with constants
+        IPPollRange ipPollRange = new IPPollRange("127.0.0.1", "127.5.118.25", 2000, 1);
+        m_ranges.add(ipPollRange);
+        DiscoveryJob discoveryJob = new DiscoveryJob(m_ranges, "Bogus FS", "Bogus Location");
+        assertTrue(Integer.MAX_VALUE > discoveryJob.calculateTaskTimeout()); 
+    }
+
+    @Test
+    public void testTimeoutIntegerMaxValueWithDefaultTimeoutRetries() throws Exception {
+        List<IPPollRange> m_ranges = new ArrayList<IPPollRange>();
+        // TODO: Replace defaults with constants
+        IPPollRange ipPollRange = new IPPollRange("127.0.0.1", "127.5.118.26", 2000, 1);
+        m_ranges.add(ipPollRange);
+        DiscoveryJob discoveryJob = new DiscoveryJob(m_ranges, "Bogus FS", "Bogus Location");
+        assertEquals(Integer.MAX_VALUE, discoveryJob.calculateTaskTimeout()); 
     }
 }
