841feb37d33dc1c05bd0b424dbd885218742c7f6
Pradeep S <pradeep.s1@cerner.com>
2016-04-22 22:30:40 +0530

    HZN-655 Renamed PollerIntegrationIT to PollerMonitorsIntegrationIT

--- opennms-services/src/test/java/org/opennms/netmgt/poller/PollerRoutingTest.java
+++ opennms-services/src/test/java/org/opennms/netmgt/poller/PollerRoutingTest.java
@@ -2,90 +2,102 @@ package org.opennms.netmgt.poller;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentSkipListMap;
 
-import org.apache.camel.CamelContext;
-import org.apache.camel.ProducerTemplate;
+import org.apache.camel.builder.AdviceWithRouteBuilder;
 import org.apache.camel.builder.RouteBuilder;
-import org.apache.camel.spring.SpringCamelContext;
-import org.junit.Before;
+import org.apache.camel.impl.JndiRegistry;
+import org.apache.camel.model.RouteDefinition;
+import org.apache.camel.test.junit4.CamelTestSupport;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opennms.core.test.OpenNMSJUnit4ClassRunner;
-import org.opennms.core.test.db.MockDatabase;
-import org.opennms.core.test.db.annotations.JUnitTemporaryDatabase;
 import org.opennms.netmgt.poller.mock.MockMonitoredService;
-import org.opennms.test.JUnitConfigurationEnvironment;
+import org.opennms.netmgt.poller.monitors.AvailabilityMonitor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
+import org.opennms.core.soa.ServiceRegistry;
 
 @RunWith( OpenNMSJUnit4ClassRunner.class )
-@ContextConfiguration( locations = { 
-		"classpath:/META-INF/opennms/applicationContext-soa.xml",
-		"classpath:/META-INF/opennms/applicationContext-pollerd.xml",
-        "classpath:/META-INF/opennms/applicationContext-dao.xml",
-        "classpath:/META-INF/opennms/applicationContext-minimal-conf.xml",
-        "classpath*:/META-INF/opennms/component-dao.xml",
-        "classpath*:/META-INF/opennms/component-service.xml",
-        "classpath:/META-INF/opennms/applicationContext-daemon.xml",
-        "classpath:/META-INF/opennms/applicationContext-eventUtil.xml",
-        "classpath:/META-INF/opennms/mockEventIpcManager.xml",
-        "classpath:/META-INF/opennms/applicationContext-commonConfigs.xml",
-        "classpath:/META-INF/opennms/applicationContext-testMockEventIpcManager.xml"
-		} )
-
-@JUnitConfigurationEnvironment 
-@JUnitTemporaryDatabase(tempDbClass=MockDatabase.class,reuseDatabase=false)
-public class PollerRoutingTest extends RouteBuilder {
-
+@ContextConfiguration( locations = { "classpath:/META-INF/opennms/emptyContext.xml",
+									 "classpath:/META-INF/opennms/applicationContext-soa.xml"} )
+public class PollerRoutingTest extends CamelTestSupport {
+	
 	@Autowired
-	ApplicationContext context;
+	ServiceRegistry m_serviceregistry;
 	
+	@Override
+    protected JndiRegistry createRegistry() throws Exception
+    {
+        JndiRegistry registry = super.createRegistry();
+
+        registry.bind( "availabilityMonitor", new AvailabilityMonitor() );
+        registry.bind( "availabilityMonitorCamel", new ServiceMonitorCamelImpl("direct:pollAvailabilityMonitor",new AvailabilityMonitor() ) );
+
+        return registry;
+    }
 	
-	@Before
-	public void configure() throws Exception {
-		 {
-             // Add exception handlers
-             onException( IOException.class ).handled( true ).logStackTrace( true ).stop();
+	/**
+     * Delay calling context.start() so that you can attach an {@link AdviceWithRouteBuilder} to the
+     * context before it starts.
+     */
+    @Override
+    public boolean isUseAdviceWith()
+    {
+        return true;
+    }
+    
+    
+    /**
+     * Build the route for all of the config parsing messages.
+     */
+    @Override
+    protected RouteBuilder createRouteBuilder() throws Exception
+    {
+        return new RouteBuilder() {
 
-             from( "direct:pollAvailabilityMonitor" ).setHeader("CamelJmsRequestTimeout", simple("40000",Long.class)).to( "bean:availabilityMonitorCamel" ).split( body() ).recipientList(
-                             simple( "seda:Location-${body.location}.Poller.AvailabilityMonitor" ) );
+            @Override
+            public void configure() throws Exception
+            {
+                // Add exception handlers
+                onException( IOException.class ).handled( true ).logStackTrace( true ).stop();
 
-             from( "seda:Location-localhost.Poller.AvailabilityMonitor" ).to( "bean:availabilityMonitor?method=poll" );
-             
-         }
-		
-	}
-	
-	@Test
-	public void testPoller() throws Exception{
-		CamelContext camelContext = null;
-	       try {
-			camelContext = SpringCamelContext.springCamelContext(
+                from( "direct:pollAvailabilityMonitor" ).setHeader("CamelJmsRequestTimeout", simple("40000",Long.class)).to( "bean:availabilityMonitorCamel" ).split( body() ).recipientList(
+                                simple( "seda:Location-${body.location}.Poller.AvailabilityMonitor" ) );
 
-					   context, false);
+                //from( "seda:Location-localhost.Poller.AvailabilityMonitor" ).to( "bean:availabilityMonitor" );
+            }
+        };
+    }
+    
 
+    @Test
+    public void testPoller() throws Exception
+    {
+    	
+        for ( RouteDefinition route : new ArrayList<RouteDefinition>( context.getRouteDefinitions() ) )
+        {
+            route.adviceWith( context, new AdviceWithRouteBuilder() {
+                @Override
+                public void configure() throws Exception
+                {
+                    mockEndpoints();
+                }
+            } );
+        }
+        context.start();
 
-		            camelContext.start();
+        MonitoredServiceTask monitoredServiceTask  = new MonitoredServiceTask();
+        monitoredServiceTask.setMonitoredService(new MockMonitoredService(1, "wipv6day.opennms.org", InetAddress.getLocalHost(), "RESOLVE"));
+        monitoredServiceTask.setParameters(new HashMap<String, Object>());
 
-		            ProducerTemplate template = camelContext.createProducerTemplate();
+        // Execute the job
+        template.requestBody( "direct:pollAvailabilityMonitor", monitoredServiceTask );
 
-		            MonitoredServiceTask monitoredServiceTask  = new MonitoredServiceTask();
-		            monitoredServiceTask.setMonitoredService(new MockMonitoredService(1, "wipv6day.opennms.org", InetAddress.getLocalHost(), "RESOLVE"));
-		            monitoredServiceTask.setParameters(new HashMap<String, Object>());
-
-		            template.sendBody( "direct:pollAvailabilityMonitor", monitoredServiceTask);
-
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	       finally {
-	            camelContext.stop();
-
-	        }
-
-	}
+        assertMockEndpointsSatisfied();
+    }
 
 }
