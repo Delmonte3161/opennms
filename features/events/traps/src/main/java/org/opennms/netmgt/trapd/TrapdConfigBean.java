04f1d5c386ef8b41fcb7208eabd7cd512165aa12
Malatesh Sudarshan <Malatesh.Sudarshan@cerner.com>
2016-05-17 12:47:48 +0530

    Added jaxb annotations for TrapdConfigBean and snmpv3users elements in trapd-configuration.xml

--- features/events/traps/src/main/java/org/opennms/netmgt/trapd/TrapdConfigBean.java
+++ features/events/traps/src/main/java/org/opennms/netmgt/trapd/TrapdConfigBean.java
@@ -30,6 +30,16 @@ package org.opennms.netmgt.trapd;
 
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+
+import org.eclipse.persistence.jaxb.JAXBMarshaller;
+import org.opennms.core.camel.JaxbUtilsUnmarshalProcessor;
+import org.opennms.core.network.InetAddressXmlAdapter;
 import org.opennms.netmgt.config.TrapdConfig;
 import org.opennms.netmgt.snmp.SnmpV3User;
 
@@ -39,11 +49,15 @@ import org.opennms.netmgt.snmp.SnmpV3User;
  * 
  * @author dp044946
  */
+@XmlRootElement(name="trapd-configuration")
+@XmlAccessorType(XmlAccessType.NONE)
 public class TrapdConfigBean implements TrapdConfig {
 
 	private String m_snmpTrapAddress;
 	private int m_snmpTrapPort;
 	private boolean m_newSuspectOnTrap;
+	
+	@XmlElement(required=true)
 	private List<SnmpV3User> m_snmpV3Users;
 
 	public void setSnmpTrapAddress(String snmpTrapAddress) {
@@ -78,6 +92,7 @@ public class TrapdConfigBean implements TrapdConfig {
 	}
 
 	@Override
+	@XmlElement(name="snmpv3users")
 	public List<SnmpV3User> getSnmpV3Users() {
 		return m_snmpV3Users;
 	}
