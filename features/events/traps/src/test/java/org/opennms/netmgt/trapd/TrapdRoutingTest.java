3ec60e693ebd0f39762cce1c706401ec53d369e5
Pavan Kumar RP <pavan.kumarrp@cerner.com>
2016-05-19 18:18:54 +0530

    HZN-750: Initial check-in of JUnit

---
+++ features/events/traps/src/test/java/org/opennms/netmgt/trapd/TrapdRoutingTest.java
@@ -0,0 +1,77 @@
+package org.opennms.netmgt.trapd;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.camel.builder.AdviceWithRouteBuilder;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.JndiRegistry;
+import org.apache.camel.model.RouteDefinition;
+import org.apache.camel.test.junit4.CamelTestSupport;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.opennms.core.camel.JaxbUtilsUnmarshalProcessor;
+import org.opennms.core.test.OpenNMSJUnit4ClassRunner;
+import org.springframework.test.context.ContextConfiguration;
+
+@RunWith(OpenNMSJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:/META-INF/opennms/emptyContext.xml" })
+public class TrapdRoutingTest extends CamelTestSupport {
+
+	@Override
+    protected JndiRegistry createRegistry() throws Exception
+    {
+        JndiRegistry registry = super.createRegistry();
+
+        registry.bind( "trapd", new TrapdConfigBean() );
+        registry.bind("unmarshaller", new JaxbUtilsUnmarshalProcessor("org.opennms.netmgt.trapd.TrapdConfigBean"));
+
+        return registry;
+    }
+
+	/**
+	 * Delay calling context.start() so that you can attach an
+	 * {@link AdviceWithRouteBuilder} to the context before it starts.
+	 */
+	@Override
+	public boolean isUseAdviceWith() {
+		return true;
+	}
+
+	/**
+	 * Build the route for all of the config parsing messages.
+	 */
+	@Override
+	protected RouteBuilder createRouteBuilder() throws Exception {
+		return new RouteBuilder() {
+
+			@Override
+			public void configure() throws Exception {
+				// Add exception handlers
+				onException(IOException.class).handled(true)
+						.logStackTrace(true).stop();
+
+				from("direct:http://localhost:8980/opennms/rest/config/trapd")
+						.beanRef("unmarshaller")
+						.to("bean:trapd?method=onSnmpV3UsersUpdate")
+						.split(body())
+						.recipientList(simple("seda:Location-${body.location}"));
+
+			}
+		};
+	}
+
+	@Test
+	public void testTrapRouting() throws Exception {
+		for (RouteDefinition route : new ArrayList<RouteDefinition>(
+				context.getRouteDefinitions())) {
+			route.adviceWith(context, new AdviceWithRouteBuilder() {
+				@Override
+				public void configure() throws Exception {
+					mockEndpoints();
+				}
+			});
+		}
+		context.start();
+	}
+}
