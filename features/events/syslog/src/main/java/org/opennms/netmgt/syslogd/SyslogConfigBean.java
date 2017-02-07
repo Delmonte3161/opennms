bc13bd55d746f2952dcbcc08c9b3541cd59d7c07
Pradeep S <pradeep.s1@cerner.com>
2017-02-07 12:13:20 +0530

    Harcoded the number fo threads to 1

--- features/events/syslog/src/main/java/org/opennms/netmgt/syslogd/SyslogConfigBean.java
+++ features/events/syslog/src/main/java/org/opennms/netmgt/syslogd/SyslogConfigBean.java
@@ -136,7 +136,8 @@ public final class SyslogConfigBean implements SyslogdConfig {
     @Override
     public int getNumThreads() {
         if (m_numThreads <= 0) {
-            return Runtime.getRuntime().availableProcessors() * 2;
+           // return Runtime.getRuntime().availableProcessors() * 2;
+        	return 1;
         }
         return m_numThreads;
     }
