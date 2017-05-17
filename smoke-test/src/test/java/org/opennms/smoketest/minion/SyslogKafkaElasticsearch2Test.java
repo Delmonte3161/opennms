2330f8aedca472e69d08775493b5af741f670a6f
Pradeep S <pradeep.s1@cerner.com>
2017-05-17 15:20:21 +0530

    Moved the warm syslogs after the reset stats and increased the sleep time

--- smoke-test/src/test/java/org/opennms/smoketest/minion/SyslogKafkaElasticsearch2Test.java
+++ smoke-test/src/test/java/org/opennms/smoketest/minion/SyslogKafkaElasticsearch2Test.java
@@ -98,8 +98,6 @@ public class SyslogKafkaElasticsearch2Test extends AbstractSyslogTest {
 
         LOG.info("Warming up syslog routes by sending 100 packets");
 
-        // Warm up the routes
-        sendMessage(ContainerAlias.MINION, sender, 100);
 
         for (int i = 0; i < 10; i++) {
             LOG.info("Slept for " + i + " seconds");
@@ -108,10 +106,12 @@ public class SyslogKafkaElasticsearch2Test extends AbstractSyslogTest {
 
         LOG.info("Resetting statistics");
         resetRouteStatistics(opennmsSshAddr, minionSshAddr);
+        // Warm up the routes
+        sendMessage(ContainerAlias.MINION, sender, 100);
 
         for (int i = 0; i < 20; i++) {
             LOG.info("Slept for " + i + " seconds");
-            Thread.sleep(1000);
+            Thread.sleep(2000);
         }
 
         // Make sure that this evenly divides into the numMessages
