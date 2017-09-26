bdc2af2f40d1170b5764a2bac5d6e97952b9bc3e
tf016851 <tim.fite@cerner.com>
2017-09-25 22:04:15 -0500

    Creating ACI branch off of 20.0.2 release tag.  Incrementing to version 20.0.3

---
+++ integrations/opennms-aci/README.md
@@ -0,0 +1,12 @@
+
+## Running
+
+```
+features:install json-simple
+install mvn:org.opennms/opennms-config-model/21.0.0-SNAPSHOT
+install mvn:org.opennms.aci/opennms-aci-config/21.0.0-SNAPSHOT
+install mvn:org.opennms.aci/aci-rest/21.0.0-SNAPSHOT
+install mvn:org.opennms.aci/org.opennms.aci.rpc.commands/21.0.0-SNAPSHOT
+install mvn:org.opennms.aci/opennms-aci.main-module/21.0.0-SNAPSHOT
+install mvn:org.opennms.aci/opennms-aci-provision/21.0.0-SNAPSHOT
+```
---
+++ integrations/opennms-aci/provision/src/main/java/org/opennms/netmgt/provision/service/aci/AciRequisitionRequest.java
@@ -0,0 +1,122 @@
+/*******************************************************************************
+ * This file is part of OpenNMS(R).
+ *
+ * Copyright (C) 2017-2017 The OpenNMS Group, Inc.
+ * OpenNMS(R) is Copyright (C) 1999-2017 The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is a registered trademark of The OpenNMS Group, Inc.
+ *
+ * OpenNMS(R) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * OpenNMS(R) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with OpenNMS(R).  If not, see:
+ *      http://www.gnu.org/licenses/
+ *
+ * For more information contact:
+ *     OpenNMS(R) Licensing <license@opennms.org>
+ *     http://www.opennms.org/
+ *     http://www.opennms.com/
+ *******************************************************************************/
+
+package org.opennms.netmgt.provision.service.aci;
+
+import java.util.Map;
+import java.util.Objects;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import org.opennms.netmgt.provision.persist.RequisitionRequest;
+import org.opennms.netmgt.provision.persist.requisition.Requisition;
+
+/*
+ * @author mp050407
+ */
+
+@XmlRootElement(name = "aci-requisition-request")
+@XmlAccessorType(XmlAccessType.NONE)
+public class AciRequisitionRequest implements RequisitionRequest {
+
+	@XmlAttribute(name = "hostname")
+	private String hostname = null;
+
+	@XmlAttribute(name = "username")
+	private String username = null;
+
+	@XmlAttribute(name = "password")
+	private String password = null;
+
+	@XmlAttribute(name = "location")
+	private String location = null;
+
+	@XmlAttribute(name = "foreign-source")
+	private String foreignSource = null;
+
+	@SuppressWarnings("unused")
+	private Requisition existingRequisition;
+
+	public AciRequisitionRequest(Map<String, String> parameters) {
+		setHostname(parameters.get("hostname"));
+		setUsername(parameters.get("username"));
+		setPassword(parameters.get("password"));
+		setLocation(parameters.get("location"));
+	}
+
+	public AciRequisitionRequest() {
+		// TODO Auto-generated constructor stub
+	}
+
+	private void setLocation(String location) {
+		this.location = location;
+	}
+
+	private void setPassword(String password) {
+		this.password = password;
+	}
+
+	private void setUsername(String username) {
+		this.username = username;
+	}
+
+	private void setHostname(String hostname) {
+		this.hostname = hostname;
+
+	}
+
+	public String getForeignSource() {
+		return foreignSource;
+	}
+
+	public void setExistingRequisition(Requisition existingRequisition) {
+		this.existingRequisition = existingRequisition;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(hostname, username, password, location, foreignSource, existingRequisition);
+	}
+
+	@Override
+	public boolean equals(final Object other) {
+		if (!(other instanceof AciRequisitionRequest)) {
+			return false;
+		}
+		AciRequisitionRequest castOther = (AciRequisitionRequest) other;
+		return Objects.equals(hostname, castOther.hostname) && Objects.equals(username, castOther.username)
+				&& Objects.equals(password, castOther.password) && Objects.equals(location, castOther.location)
+				&& Objects.equals(foreignSource, castOther.foreignSource)
+				&& Objects.equals(existingRequisition, castOther.existingRequisition);
+	}
+
+}
+
